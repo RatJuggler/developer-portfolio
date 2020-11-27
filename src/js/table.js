2a11f94a8083a63855d2f04f78a6c8ccfed79cff
John Chase <ratteal@gmail.com>
2020-11-27 18:08:17 +0000

    Experiment with table buttons.

---
+++ src/js/table.js
@@ -0,0 +1,24 @@
+function buttons () {
+    return {
+        clearFilters: {
+            text: 'Clear filters',
+            icon: 'fa-eraser',
+            event: function () {
+                $('#skillsTable').bootstrapTable('clearFilterControl');
+            },
+            attributes: {
+                title: 'Clear all column filters'
+            }
+        },
+        addSkill: {
+            text: 'Add a new skill',
+            icon: 'fa-plus',
+            event: function () {
+                alert('TODO: Add a new skill row!')
+            },
+            attributes: {
+                title: 'Add a new row to the table'
+            }
+        }
+    }
+}
