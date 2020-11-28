50cb3fc875b716169eb405f1d3c3beda32a3b655
John Chase <ratteal@gmail.com>
2020-11-28 16:04:37 +0000

    Get Twig.js Hello World running.

--- src/app.js
+++ src/app.js
@@ -2,8 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.set('views', 'src/templates');
+app.set('view engine', 'twig');
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.render('index.twig', { name: 'World'});
+});
+
+app.get('/name/:name', (req, res) => {
+    res.render('index.twig', req.params);
 });
 
 app.listen(port, () => {
