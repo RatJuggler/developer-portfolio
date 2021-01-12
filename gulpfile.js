c7685219f599ea1a51e7fd76b048ec55dedb04a1
John Chase <ratteal@gmail.com>
2021-01-12 12:55:58 +0000

    Keep the static data with the webapp templates.

--- gulpfile.js
+++ gulpfile.js
@@ -119,7 +119,7 @@ function copyAppDist() {
         .pipe(replace('.js', '.min.js'))
         .pipe(gulp.dest('dist/app/templates'));
     // Application Data
-    let appData = gulp.src('./src/data/*.json')
+    let appData = gulp.src('./src/main/weapp/data/*.json')
         .pipe(gulp.dest('dist/app/data'));
     return merge(appJS, appTemplates, appData);
 }
