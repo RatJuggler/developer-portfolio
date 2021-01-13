cf43bb45b4d9ec2fe0953cd3c0984c36d69ead9a
John Chase <ratteal@gmail.com>
2021-01-13 18:20:41 +0000

    Use the Interests repository and return all entries.

--- src/main/java/com/portfolio/rest/InterestResource.java
+++ src/main/java/com/portfolio/rest/InterestResource.java
@@ -2,14 +2,26 @@ package com.portfolio.rest;
 
 import com.portfolio.domain.Interest;
 
+import com.portfolio.repository.InterestRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class InterestResource {
 
+    private final InterestRepository repository;
+
+    public InterestResource(final InterestRepository repository) {
+        this.repository = repository;
+    }
+
     @GetMapping("/interests")
-    public Interest interest() {
-        return new Interest(1, "Title", "ImagePath", "Description");
+    public List<Interest> interests() {
+        List<Interest> interests = new ArrayList<>();
+        repository.findAll().forEach(interests::add);
+        return interests;
     }
 }
