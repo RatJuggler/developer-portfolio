e214da1eb88a5beae758c446d715c51c7f7e92f9
John Chase <ratteal@gmail.com>
2021-01-13 18:55:07 +0000

    Use the Career repository and return all entries.

--- src/main/java/com/portfolio/rest/CareerResource.java
+++ src/main/java/com/portfolio/rest/CareerResource.java
@@ -2,14 +2,26 @@ package com.portfolio.rest;
 
 import com.portfolio.domain.Career;
 
+import com.portfolio.repository.CareerRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class CareerResource {
 
+    private final CareerRepository repository;
+
+    public CareerResource(final CareerRepository repository) {
+        this.repository = repository;
+    }
+
     @GetMapping("/career")
-    public Career skill() {
-        return new Career(1, 1, "Description", "ImagePath", "Synopsis");
+    public List<Career> career() {
+        List<Career> career = new ArrayList<>();
+        repository.findAll().forEach(career::add);
+        return career;
     }
 }
