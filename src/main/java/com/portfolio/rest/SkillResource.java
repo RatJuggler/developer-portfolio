bf84671586036ed0ad3594f4dc9ca7b684f39af5
John Chase <ratteal@gmail.com>
2021-01-13 18:57:53 +0000

    Use the Skill repository and return all entries.

--- src/main/java/com/portfolio/rest/SkillResource.java
+++ src/main/java/com/portfolio/rest/SkillResource.java
@@ -2,14 +2,26 @@ package com.portfolio.rest;
 
 import com.portfolio.domain.Skill;
 
+import com.portfolio.repository.SkillRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class SkillResource {
 
+    private final SkillRepository repository;
+
+    public SkillResource(final SkillRepository repository) {
+        this.repository = repository;
+    }
+
     @GetMapping("/skills")
-    public Skill skill() {
-        return new Skill(1, "Level", "Category", "Skill", "Description");
+    public List<Skill> skill() {
+        List<Skill> skills = new ArrayList<>();
+        repository.findAll().forEach(skills::add);
+        return skills;
     }
 }
