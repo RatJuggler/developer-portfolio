0347e42a5da9040f7018c39a2304e839af05cc2d
John Chase <ratteal@gmail.com>
2021-01-26 12:20:00 +0000

    Make sure we copy data for application properly.

--- gulpfile.js
+++ gulpfile.js
@@ -119,7 +119,7 @@ function copyAppDist() {
         .pipe(replace('.js', '.min.js'))
         .pipe(gulp.dest('dist/app/templates'));
     // Application Data
-    let appData = gulp.src('./src/main/webapp/data/*.json')
+    let appData = gulp.src('./src/main/resources/data/*.json')
         .pipe(gulp.dest('dist/app/data'));
     return merge(appJS, appTemplates, appData);
 }
