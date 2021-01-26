1f6aed946e720cf346f28253efcca88f40cdbc16
John Chase <ratteal@gmail.com>
2021-01-26 13:51:24 +0000

    Revert back to older working version for docker testing.

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -1,27 +1,26 @@
 const templatePath = process.argv[2];
 const dataPath = process.argv[3];
-const javaURL = process.argv[4];
+
+const fs = require("fs");
 
 const express = require('express');
 const app = express();
 const port = 3000;
-
-const fs = require('fs');
-const got = require('got');
 
 app.set('views', templatePath);
 app.set('view engine', 'twig');
 
 app.use(express.static('public'));
 
-app.get('/:dataFrom/*.twig', (req, res) => {
-    let aspect = req.params['0'];
-    let dataFrom = req.params['dataFrom'];
+const templateStatic = 'Template Version with Static JSON Data Files';
+
+app.get('/*.twig', (req, res) => {
+    let aspect = req.params["0"];
     res.render(aspect + '.twig',
         {
-            version: getTemplateVersion(dataFrom),
-            profile: getDataFrom('profile', dataFrom),
-            data: getDataFrom(aspect, dataFrom)
+            version: templateStatic,
+            profile: readJSON(dataPath + '/profile.json'),
+            data: readJSON(dataPath + '/' + aspect + '.json')
         });
 });
 
@@ -32,26 +31,4 @@ app.listen(port, () => {
 function readJSON(filepath) {
     let contents = fs.readFileSync(filepath);
     return JSON.parse(contents);
-}
-
-function getDataFrom(aspect, dataFrom) {
-    switch (dataFrom) {
-        case 'json':
-            return readJSON(dataPath + '/' + aspect + '.json');
-        case 'java':
-            return {};
-        default:
-            return {};
-    }
-}
-
-function getTemplateVersion(dataFrom) {
-    switch (dataFrom) {
-        case 'json':
-            return 'Template Version with Static JSON Data Files';
-        case 'java':
-            return  'Template Version with Static Data from Java Services';
-        default:
-            return 'Unknown data source: ' + dataFrom;
-    }
 }
