40fa44d485aafa0dd144be142ebe56b65edca50e
John Chase <ratteal@gmail.com>
2021-01-29 17:34:52 +0000

    Put template pages on a template path without using .twig.

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -13,15 +13,25 @@ app.set('view engine', 'twig');
 app.use(express.static('public'));
 
 const templateStatic = 'Template Version with Static JSON Data Files';
+const templates = ['profile', 'skills', 'career', 'interests'];
 
-app.get('/*.twig', (req, res) => {
+app.get('/', function (req, res) {
+    res.redirect('/template/profile');
+});
+
+app.get('/template/*', function (req, res, next) {
     let aspect = req.params["0"];
+    if (!templates.includes(aspect)) aspect = templates[0];
     res.render(aspect + '.twig',
         {
             version: templateStatic,
             profile: readJSON(dataPath + '/profile.json'),
             data: readJSON(dataPath + '/' + aspect + '.json')
         });
+});
+
+app.use(function (req, res) {
+    res.status(404).send("No such template!")
 });
 
 app.listen(port, () => {
