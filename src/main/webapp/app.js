756bebdbcaa40e478c6ae79c1c50513fe17d84a4
John Chase <ratteal@gmail.com>
2021-01-31 21:51:55 +0000

    Make request JSON file read and request handlers async.

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -2,7 +2,7 @@ const templatePath = process.argv[2];
 const dataPath = process.argv[3];
 
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs').promises;
 
 const express = require('express');
 const app = express();
@@ -18,24 +18,26 @@ const templates = ['profile', 'skills', 'career', 'interests'];
 app.use(express.static('public'));
 
 // Default landing page.
-app.get('/', function (req, res) {
+app.get('/', async (req, res) => {
     res.redirect('/template/' + templates[0]);
 });
 
 // Template rendering.
-app.get('/template/*', function (req, res) {
+app.get('/template/*', async (req, res) => {
     let aspect = req.params["0"];
     if (!templates.includes(aspect)) aspect = templates[0];
+    const profile = await readJSON(dataPath, templates[0]);
+    const data = await readJSON(dataPath, aspect);
     res.render(aspect + '.twig',
         {
             version: templateStatic,
-            profile: readJSON(dataPath, templates[0]),
-            data: readJSON(dataPath, aspect)
+            profile: profile,
+            data: data
         });
 });
 
 // Static file or Template not found.
-app.use(function (req, res) {
+app.use(async (req, res) => {
     res.status(404).sendFile(path.join('public', '404.html'));
 });
 
@@ -45,7 +47,8 @@ app.listen(port, () => {
 });
 
 function readJSON(dataPath, aspect) {
-    let filepath = path.join(dataPath, aspect + '.json');
-    let contents = fs.readFileSync(filepath);
-    return JSON.parse(contents);
+    const filepath = path.join(dataPath, aspect + '.json');
+    return fs.readFile(filepath)
+        .then(data => JSON.parse(data.toString()))
+        .catch(err => console.error('Failed to read data file!', err));
 }
