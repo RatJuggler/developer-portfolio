61f7d315d770690966ef74b61481b0f9fff33ec6
John Chase <ratteal@gmail.com>
2021-02-10 11:41:22 +0000

    Add some perfunctory tests.

---
+++ src/test/java/com/portfolio/rest/InterestResourceTest.java
@@ -0,0 +1,46 @@
+package com.portfolio.rest;
+
+import com.portfolio.domain.Interest;
+import com.portfolio.repository.InterestRepository;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(InterestResource.class)
+public class InterestResourceTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private InterestRepository repository;
+
+    @Test
+    public void getInterestsTest() throws Exception {
+        List<Interest> testInterests = new ArrayList<>();
+        testInterests.add(new Interest(1, "Test Title 1", "test/image1", "Test Description 1"));
+        when(repository.findAll()).thenReturn(testInterests);
+        ObjectMapper mapper = new ObjectMapper();
+        String expectedInterests = mapper.writeValueAsString(testInterests);
+        this.mockMvc.perform(get("/interests"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo(expectedInterests)));
+    }
+}
---
+++ src/test/java/com/portfolio/rest/ProfileResourceTest.java
@@ -0,0 +1,48 @@
+package com.portfolio.rest;
+
+import com.portfolio.domain.Profile;
+import com.portfolio.repository.ProfileRepository;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ProfileResource.class)
+public class ProfileResourceTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ProfileRepository repository;
+
+    @Test
+    public void getProfileTest() throws Exception {
+        List<Profile> testProfile = new ArrayList<>();
+        testProfile.add(new Profile(1, "Test Name 1", "Test Location 1", "Test Status 1",
+                "test/avatar1", "test/photo1", "Test Description 1", "test1@email.com",
+                "https://linkedin.com/test1", "https://github.com/test1", "@test1Twitter"));
+        when(repository.findAll()).thenReturn(testProfile);
+        ObjectMapper mapper = new ObjectMapper();
+        String expectedProfile = mapper.writeValueAsString(testProfile.get(0));
+        this.mockMvc.perform(get("/profile"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo(expectedProfile)));
+    }
+}
