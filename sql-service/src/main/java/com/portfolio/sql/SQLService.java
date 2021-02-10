83efb716f2bbca29517094cd4af8813241547c80
John Chase <ratteal@gmail.com>
2021-02-10 20:11:38 +0000

    Initial skeleton for a SQL data service.

---
+++ sql-service/src/main/java/com/portfolio/sql/SQLService.java
@@ -0,0 +1,12 @@
+package com.portfolio.sql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SQLService {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SQLService.class, args);
+	}
+}
