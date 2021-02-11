82e683418391f4974318478025452b0a1b21ca64
John Chase <ratteal@gmail.com>
2021-02-11 12:01:43 +0000

    Enable career data load.

--- simple-service/src/main/java/com/portfolio/simple/config/RepositoryPopulator.java
+++ simple-service/src/main/java/com/portfolio/simple/config/RepositoryPopulator.java
@@ -30,7 +30,7 @@ public class RepositoryPopulator {
         factory.setResources(new Resource[] {
                 new ClassPathResource("data/profile.json"),
 //                new ClassPathResource("data/skills.json"),
-//                new ClassPathResource("data/career.json"),
+                new ClassPathResource("data/career.json"),
                 new ClassPathResource("data/interests.json")
         });
         return factory;
