c58b077d0bb519e36d0c00b8ee55faf8d213d2df
John Chase <ratteal@gmail.com>
2021-03-08 20:49:50 +0000

    Link them up with the router.

--- portfolio-react/src/App.js
+++ portfolio-react/src/App.js
@@ -1,10 +1,30 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Profile from './Profile';
+import Skills from './Skills';
+import Career from "./Career";
+import Interests from "./Interests";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router basename="/react">
+      <div className="App">
+        <Switch>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+          <Route path="/skills">
+            <Skills />
+          </Route>
+          <Route path="/career">
+            <Career />
+          </Route>
+          <Route path="/interests">
+            <Interests />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
