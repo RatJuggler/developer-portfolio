d03546a8736dcf007d687827c6ea5f69657115de
John Chase <ratteal@gmail.com>
2021-03-09 19:34:16 +0000

    Create a fontawesome icon library.

---
+++ portfolio-react/src/fontawesome.js
@@ -0,0 +1,18 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+import { faEdit, faTrashAlt } from '@fortawesome/free-regular-svg-icons';
+import { faHome, faCog, faUser, faAt, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin, faGithubSquare, faTwitterSquare } from '@fortawesome/free-brands-svg-icons';
+
+library.add(
+    faHome,
+    faCog,
+    faUser,
+    faAt,
+    faLinkedin,
+    faGithubSquare,
+    faTwitterSquare,
+    faPlus,
+    faEdit,
+    faTrashAlt
+);
