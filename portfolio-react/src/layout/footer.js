eb498c6d1b07bf294a0ccb680efcd374e58df31f
John Chase <ratteal@gmail.com>
2021-03-09 17:48:05 +0000

    Start to flesh out the footer.

--- portfolio-react/src/layout/footer.js
+++ portfolio-react/src/layout/footer.js
@@ -2,6 +2,10 @@ const Footer = () => {
 
     return (
         <footer>
+            <div id="cookie-banner" className="alert alert-dark text-center mb-0" role="alert">
+                🍪 This website uses local storage to track if you've been told that it doesn't use cookies.
+                <button id="acceptCookieNotification" type="button" className="btn btn-primary btn-sm ml-3">Um, OK</button>
+            </div>
         </footer>
     );
 }
