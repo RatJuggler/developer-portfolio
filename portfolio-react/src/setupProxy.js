9c3c98d0a5398a38844ffb672922024c33f145e4
John Chase <ratteal@gmail.com>
2021-03-17 09:54:10 +0000

    Rewrite paths to fix accesss url.

--- portfolio-react/src/setupProxy.js
+++ portfolio-react/src/setupProxy.js
@@ -1,7 +1,16 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-    app.use("/json", createProxyMiddleware("http://localhost:8000"));
-    app.use("/map", createProxyMiddleware("http://localhost:8001"));
-    app.use("/sql", createProxyMiddleware("http://localhost:8002"));
+    app.use("/json", createProxyMiddleware({
+        target: "http://localhost:8000"
+    }));
+    app.use("/map", createProxyMiddleware({
+        target: "http://localhost:8001",
+        pathRewrite: function (path, req) { return path.replace("/map", "") }
+    }));
+    app.use("/sql", createProxyMiddleware({
+        target: "http://localhost:8002",
+        pathRewrite: function (path, req) { return path.replace("/sql", "") }
+
+    }));
 };
